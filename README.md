bfe4fd9060af3de5a91dc6254834dcccd01cc621
Paul Wallrabe <54737071+raballew@users.noreply.github.com>
2022-06-10 17:56:03 +0200

    replace file_ops with proc_ops

--- README.md
+++ README.md
@@ -4,24 +4,24 @@ Simple kmod example taken from [tldp.org](https://www.tldp.org/LDP/lkmpg/2.6/htm
 
 To build on Fedora, first install:
 
-```
+```bash
 sudo dnf install kernel-devel make gcc
 ```
 
 To build for the currently running kernel use:
 
-```
+```bash
 make all
 ```
 
 To build for a specific kernel:
 
-```
+```bash
 make all KVER=5.3.4-300.fc31.x86_64
 ```
 
 Load module:
 
-```
+```bash
 sudo insmod hello-1.ko
 ```
--- simple-procfs-kmod.c
+++ simple-procfs-kmod.c
@@ -6,14 +6,14 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/init.h>
-#include <linux/kernel.h>   
+#include <linux/kernel.h>
 #include <linux/proc_fs.h>
 #include <asm/uaccess.h>
-#define BUFSIZE  100
+#define BUFSIZE 100
 
 #define PROCFS_NAME "simple-procfs-kmod"
 
-// Set module version 
+// Set module version
 MODULE_VERSION(KMODVER);
 
 // Code taken from https://github.com/dev-area/proctest
@@ -24,21 +24,21 @@ MODULE_VERSION(KMODVER);
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Liran B.H");
 
-static int number=0;
-module_param(number,int,0660);
+static int number = 0;
+module_param(number, int, 0660);
 
 static struct proc_dir_entry *ent;
 
-static ssize_t mywrite(struct file *file, const char __user *ubuf,size_t count, loff_t *ppos) 
+static ssize_t mywrite(struct file *file, const char __user *ubuf, size_t count, loff_t *ppos)
 {
-    int num,c,i;
+    int num, c, i;
     char buf[BUFSIZE];
-    if(*ppos > 0 || count > BUFSIZE)
+    if (*ppos > 0 || count > BUFSIZE)
         return -EFAULT;
-    if(raw_copy_from_user(buf,ubuf,count))
+    if (raw_copy_from_user(buf, ubuf, count))
         return -EFAULT;
-    num = sscanf(buf,"%d",&i);
-    if(num != 1)
+    num = sscanf(buf, "%d", &i);
+    if (num != 1)
         return -EFAULT;
     number = i;
     c = strlen(buf);
@@ -46,30 +46,27 @@ static ssize_t mywrite(struct file *file, const char __user *ubuf,size_t count, 
     return c;
 }
 
-static ssize_t myread(struct file *file, char __user *ubuf,size_t count, loff_t *ppos) 
+static ssize_t myread(struct file *file, char __user *ubuf, size_t count, loff_t *ppos)
 {
     char buf[BUFSIZE];
-    int len=0;
-    if(*ppos > 0 || count < BUFSIZE)
+    int len = 0;
+    if (*ppos > 0 || count < BUFSIZE)
         return 0;
-    len += sprintf(buf,"%s number = %d\n",PROCFS_NAME, number);
-    
-    if(raw_copy_to_user(ubuf,buf,len))
+    len += sprintf(buf, "%s number = %d\n", PROCFS_NAME, number);
+
+    if (raw_copy_to_user(ubuf, buf, len))
         return -EFAULT;
     *ppos = len;
     return len;
 }
 
-static struct file_operations myops = 
-{
-    .owner = THIS_MODULE,
-    .read = myread,
-    .write = mywrite,
-};
+static struct proc_ops myops = {
+    .proc_read = seq_read,
+    .proc_write = my_proc_write};
 
 static int simple_init(void)
 {
-    ent=proc_create(PROCFS_NAME,0660,NULL,&myops);
+    ent = proc_create(PROCFS_NAME, 0660, NULL, &myops);
     printk(KERN_INFO "%s: hello ...\n", PROCFS_NAME);
     return 0;
 }
